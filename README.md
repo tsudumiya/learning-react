12be9d9ac06fd01a32539bffee17f06c4168df4b
tsudumiya <tsudumiya0105@yahoo.co.jp>
2022-03-23 09:27:01 +0900

    edit file

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ## index
 - react-hol-4
 - react-hol-5
+- react-hol-6
 - react-hol-8
--- react-hol-5/5-4/src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import data from './data/recipes.json';
-import Menu from './components/Menu';
-
-export default function App() {
-    return <Menu recipes={data} />;
-}
